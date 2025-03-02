89742b7473e5d34f31229bd728e0d413eb3e97aa
Minerva101 <cascorreia96@gmail.com>
2025-03-02 02:47:35 +0000

    estilos corrigidos, tudo para trás está testado e bem feito, o botão já aparece à esquerda e a nav bar já está no main mas inda está por implementar

--- DIAM-Semana3/vilar_de_mouros/src/Pages/Header.js
+++ DIAM-Semana3/vilar_de_mouros/src/Pages/Header.js
@@ -4,7 +4,6 @@ import fotoHeaderBig from '../Images/fotoHeaderBig.jpg';
 import fotoHeaderSmall from '../Images/fotoHeaderSmall.png';
 import { useState } from "react"; // para poder usar o estado
 
-
 function Header() {
 
     const [isHidden, setIsHidden] = useState(false);     // Inicialmente, a imagem pequena não está oculta
@@ -24,48 +23,53 @@ function Header() {
                     position: "relative",
                 }}
             >
-                <div>
-                    <img
-                        src={fotoHeaderBig}
-                        className="fotoHeaderBig"
-                        alt="fotoHeaderBig"
-                        style={{ width: "100%" }}
-                    />
-                </div>
-
-                {/* Se a foto pequena estiver visível */}
-                {!isHidden && (
-                    <div style={{ position: "absolute", display: "flex" }}>
-                        <img
-                            src={fotoHeaderSmall}
-                            className="fotoHeaderSmall"
-                            alt="fotoHeaderSmall"
-                            onMouseOver={hideImg}   // Quando o rato passa por cima da foto pequena e ela deixa de estar visível
-                            style={{ cursor: "pointer" }} // Fancy eheheh, só mostar que o botão é reativo
-                        />
-                    </div>
-                )}
+            <div>
+                <img
+                    src={fotoHeaderBig}
+                    className="fotoHeaderBig"
+                    alt="fotoHeaderBig"
+                    style={{ width: "100%" }}
+                />
             </div>
 
+            {/* Se a foto pequena estiver visível */}
+            {!isHidden && (
+                <div style={{ position: "absolute", display: "flex" }}>
+                    <img
+                        src={fotoHeaderSmall}
+                        className="fotoHeaderSmall"
+                        alt="fotoHeaderSmall"
+                        onMouseOver={hideImg}   // Quando o rato passa por cima da foto pequena e ela deixa de estar visível
+                        style={{ cursor: "pointer" }} // Fancy eheheh, só mostar que o botão é reativo
+                    />
+                </div>
+            )}
+                    
             {/* Se a foto pequena estiver oculta, aparece o botão */}
             {isHidden && (
                 <button
-                    id="hide-btn"
-                    type="button"
-                    onClick={unhideImg} // Quando se clica no botão e a foto pequena reaparece
-                    style={{
-                        marginTop: "10px",
-                        padding: "8px 12px",
-                        fontSize: "16px",
-                        cursor: "pointer",  // Só mostar que o botão é reativo
-                    }}
-                >
+                        id="hide-btn"
+                        type="button"
+                        onClick={unhideImg} // Quando se clica no botão e a foto pequena reaparece
+                        style={{
+                            marginTop: "10px",
+                            padding: "8px 12px",
+                            fontSize: "16px",
+                            cursor: "pointer",  // Só mostar que o botão é reativo
+                        }}
+                    >
                     Mostrar imagem
                 </button>
             )}
+            </div>
+
+            <div>
+                <h2 className="mainHeading"> Bem-vindo ao festival de música mais antigo do país! </h2>
+            </div>
 
         </div>
-    );
+   );  
+
 }
 
 export default Header;
--- DIAM-Semana3/vilar_de_mouros/src/Pages/Main.js
+++ DIAM-Semana3/vilar_de_mouros/src/Pages/Main.js
@@ -1,8 +1,24 @@
+import './styles.css';
 
 function Main(){
     return(
         <div className="Main">
-            
+
+            {/* Página Incial */}
+            <div>
+                
+                {/* Página Incial */}
+                <ul>
+                    <li><a href="home.html">Página Inicial</a></li>
+                    <li><a href="about.html">Sobre</a></li>
+                    <li><a href="lineUp.html">Cartaz</a></li>
+                    <li><a href="volunteersForm.html">Voluntariado</a></li>
+                    <li><a href="latestNews.html">Últimas Notícias</a></li>
+                    <li><a href="contacts.html">Contactos</a></li>
+                    <li><a href="complaints.html">Reclamações</a></li>
+                </ul>
+        
+            </div>
         </div>
     );
 }
