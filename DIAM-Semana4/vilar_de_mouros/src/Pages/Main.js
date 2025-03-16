cc7f0f957a8f347067875c94055f66b0c11ae8fd
Misael Armando <misas.armando@gmail.com>
2025-03-16 21:48:47 +0000

    All changes done

--- DIAM-Semana4/vilar_de_mouros/src/Pages/Main.js
+++ DIAM-Semana4/vilar_de_mouros/src/Pages/Main.js
@@ -1,43 +1,49 @@
 import './styles.css';
+import '../components/Artist'
+import Artist from '../components/Artist';
+import artists from '../artists.json'
 
-function Main(){
-    const scrollToSection = (id) => {
-        const section = document.getElementById(id);
-        if (section) {
-            section.scrollIntoView({ behavior: "smooth" });
-        }
-    };
+function Main() {
 
-    return(
-        <div className="MainBox">
+  return (
+    <div className="MainBox">
 
-            {/* ------------------------ Página Incial ------------------------ */}
+      <h1>Artistas</h1>
 
-            <div id="Home">
-                <div id="nav_bar">
-                    <ul>
-                        <li><a href="#Home" onClick={(e) => { e.preventDefault(); scrollToSection("Home"); }}>Página Inicial</a></li>
-                        <li><a href="#lineUp" onClick={(e) => { e.preventDefault(); scrollToSection("lineUp"); }}>Cartaz</a></li>
-                        <li><a href="#contacts" onClick={(e) => { e.preventDefault(); scrollToSection("contacts"); }}>Contactos</a></li>
-                    </ul>
-                </div>
-            </div>
-            <br/>
-                
-            {/* -------------------------- Contactos -------------------------- */}
+      {
+        // Check if exists an artist in the json file
+        artists.length == 0 ?
+          <p>Lamentos mas de momento não existem artistas...</p>
+          :
 
-            <div id ="contacts">
-                <h2> Contactos </h2>
-                <p>Email: <a href="mailto:info@festivalvilardemouros.pt">info@festivalvilardemouros.pt</a></p>
-                <p>Telefone: <a href="tel:+351 123 456 789">+351 123 456 789</a></p>
-                <p>Morada: Rua do Festival, 123, 4910-123 Vilar de Mouros</p>   
-            </div>
-            <br/>
+          // load all the artists in the file
+          artists.map((item, i) => {
+            return <>
+              <hr></hr>
+              <Artist
+                key={i}
+                nome={item.nome}
+                atuacao={item.atuacao}
+                imagem={item.imagem}
+                estilo={item.estilo}
+                descricao={item.descricao}
+                link={item.link}
+              ></Artist>
+            </>
+          })
+      }
 
-            
-        </div>
+      {/* -------------------------- Contactos -------------------------- */}
+      <div id="contacts">
+        <h2> Contactos </h2>
+        <p>Email: <a href="mailto:info@festivalvilardemouros.pt">info@festivalvilardemouros.pt</a></p>
+        <p>Telefone: <a href="tel:+351 123 456 789">+351 123 456 789</a></p>
+        <p>Morada: Rua do Festival, 123, 4910-123 Vilar de Mouros</p>
+      </div>
 
-    );
+    </div>
+
+  );
 }
 
 export default Main;
--- DIAM-Semana4/vilar_de_mouros/src/components/Artist.js
+++ DIAM-Semana4/vilar_de_mouros/src/components/Artist.js
@@ -1,10 +1,35 @@
-export default function Artist() {
+import './Artists.css'
+
+let isVideoEnable = false;
+
+const toggleVideo = (nome) => {
+  const elem = document.getElementById("youtubeVideo_" + nome);
+
+  isVideoEnable = !isVideoEnable;
+
+  elem.style.display = isVideoEnable ? "block" : "none"
+
+}
+
+export default function Artist({ nome, imagem, estilo, descricao, link, atuacao }) {
   return (
-    <>
-      <p>Nome: {nome}</p>
-      <img src={imagem}/>
-      <p>Estilo musical: {estilo}</p>
+    <section className="artistContainer">
+      <h3>Nome: {nome}</h3>
+      <p><b>Atuação:</b> {atuacao}</p>
+      <img src={imagem} alt="" width={500} onClick={() => toggleVideo(nome)} />
+      <p><b>Estilo musical:</b> {estilo}</p>
       <p>{descricao}</p>
-    </>
+
+      <iframe
+        id={'youtubeVideo_' + nome}
+        width={600}
+        height={400}
+        src={link}
+        allowFullScreen
+        style={{ display: "none" }}
+      >
+      </iframe>
+
+    </section>
   );
 }
