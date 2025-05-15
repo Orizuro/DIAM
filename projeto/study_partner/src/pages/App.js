2931ba201e8cd66ec2874eff4114d6ad81828b81
MinervaCC <cascorreia96@gmail.com>
2025-05-15 03:18:00 +0100

    componentes e páginas pensados

--- projeto/study_partner/src/pages/App.js
+++ projeto/study_partner/src/pages/App.js
@@ -1,24 +1,21 @@
-import logo from '../images/logo.svg';
 import './App.css';
+import './styles.css'
+import Header from "./Header";
+import MainPage from "./MainPage";
+import Footer from "./Footer";
+
+function ArtistsPage() {
+  return null;
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Header />
+        <MainPage />
+        <Footer />
+      </div>
   );
 }
 
---
+++ projeto/study_partner/src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import ISCTE_logo from "../images/ISCTE_logo.jpg";
+
+function MainPage() {
+
+    return ( "oalla"
+
+    );
+
+}
+
+export default MainPage;
